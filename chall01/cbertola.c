3685be3b4d07020972f4c0fbf5a9596ba29bdeb3
cyrille bertola <cyrillebertola@macbook-pro-de-cyrille.home>
2020-04-13 14:11:41 +0200

    repush

--- chall01/cbertola.c
+++ chall01/cbertola.c
@@ -6,7 +6,7 @@
 /*   By: cbertola <cyrille.bertola@student.42.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/13 14:10:33 by cbertola          #+#    #+#             */
-/*   Updated: 2020/04/13 14:11:04 by cbertola         ###   ########.fr       */
+/*   Updated: 2020/04/13 14:11:28 by cbertola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 char		 *ft_rgb2hex(int r, int g, int b)
 {
 	char *hexa;
+	
 	r = (int)pow(256, 2) * r;
 	g = 256 * g;
 	*hexa = r + g + b;
